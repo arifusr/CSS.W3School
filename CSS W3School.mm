3eb981ac5273e545b7899e22574e8a69cbf354a2
arifusr <arifusr@gmail.com>
2020-11-23 13:48:30 +0700

    CSS HOME get certificate

--- CSS W3School.mm
+++ CSS W3School.mm
@@ -2,7 +2,7 @@
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
 <node TEXT="CSS W3School" FOLDED="false" ID="ID_1694561198" CREATED="1606113482664" MODIFIED="1606113611688" STYLE="oval">
 <font SIZE="18"/>
-<hook NAME="MapStyle">
+<hook NAME="MapStyle" zoom="1.32">
     <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
 
 <map_styles>
@@ -63,6 +63,68 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="CSS Tutorial" POSITION="right" ID="ID_1839395304" CREATED="1606113666780" MODIFIED="1606113667877">
+<edge COLOR="#ff0000"/>
+<node TEXT="CSS HOME" ID="ID_1945899480" CREATED="1606113680728" MODIFIED="1606113690691">
+<node TEXT="CSS describes how HTML elements should be displayed" ID="ID_1809446193" CREATED="1606113719299" MODIFIED="1606113719745"/>
+<node TEXT="Examples in Each Chapter" ID="ID_147632967" CREATED="1606113727505" MODIFIED="1606113727900">
+<node TEXT="you can edit the CSS, and click on a button to view the result" ID="ID_617488877" CREATED="1606113747504" MODIFIED="1606113748062">
+<node TEXT="css" ID="ID_903496298" CREATED="1606113758957" MODIFIED="1606113759860">
+<node ID="ID_1946618753" CREATED="1606113760637" MODIFIED="1606113760637"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <span style="color: rgb(165, 42, 42); font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none"><font color="rgb(165, 42, 42)" face="Consolas, courier new" size="15px">body</font></span><font color="rgb(165, 42, 42)" face="Consolas, courier new" size="15px"><span>&nbsp;</span></font><span class="cssdelimitercolor" style="color: black; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="black" face="Consolas, courier new" size="15px">{</font></span><span class="csspropertycolor" style="color: red; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="red" face="Consolas, courier new" size="15px"><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"/>&nbsp;&nbsp;background-color</font></span><span class="cssdelimitercolor" style="color: black"><font color="black" face="Consolas, courier new" size="15px">:</font></span><font color="black" face="Consolas, courier new" size="15px"><span>&nbsp;</span><span class="csspropertyvaluecolor" style="color: black">lightblue</span><span class="cssdelimitercolor" style="color: black">;</span></font><span class="csspropertycolor" style="color: red; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="red" face="Consolas, courier new" size="15px"><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"/></font></span><span class="cssdelimitercolor" style="color: black; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="black" face="Consolas, courier new" size="15px">}</font></span><font color="rgb(165, 42, 42)" face="Consolas, courier new" size="15px"><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"/><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"/><span style="color: rgb(165, 42, 42); font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none">h1</span><span>&nbsp;</span></font><span class="cssdelimitercolor" style="color: black; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="black" face="Consolas, courier new" size="15px">{</font></span><span class="csspropertycolor" style="color: red; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="red" face="Consolas, courier new" size="15px"><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"/>&nbsp;&nbsp;color</font></span><span class="cssdelimitercolor" style="color: black"><font color="black" face="Consolas, courier new" size="15px">:</font></span><font color="black" face="Consolas, courier new" size="15px"><span>&nbsp;</span><span class="csspropertyvaluecolor" style="color: black">white</span><span class="cssdelimitercolor" style="color: black">;</span></font><span class="csspropertycolor" style="color: red; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="red" face="Consolas, courier new" size="15px"><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"/>&nbsp;&nbsp;text-align</font></span><span class="cssdelimitercolor" style="color: black"><font color="black" face="Consolas, courier new" size="15px">:</font></span><font color="black" face="Consolas, courier new" size="15px"><span>&nbsp;</span><span class="csspropertyvaluecolor" style="color: black">center</span><span class="cssdelimitercolor" style="color: black">;</span></font><span class="csspropertycolor" style="color: red; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="red" face="Consolas, courier new" size="15px"><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"/></font></span><span class="cssdelimitercolor" style="color: black; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="black" face="Consolas, courier new" size="15px">}</font></span><font color="rgb(165, 42, 42)" face="Consolas, courier new" size="15px"><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"/><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"/><span style="color: rgb(165, 42, 42); font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none">p</span><span>&nbsp;</span></font><span class="cssdelimitercolor" style="color: black; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="black" face="Consolas, courier new" size="15px">{</font></span><span class="csspropertycolor" style="color: red; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="red" face="Consolas, courier new" size="15px"><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"/>&nbsp;&nbsp;font-family</font></span><span class="cssdelimitercolor" style="color: black"><font color="black" face="Consolas, courier new" size="15px">:</font></span><font color="black" face="Consolas, courier new" size="15px"><span>&nbsp;</span><span class="csspropertyvaluecolor" style="color: black">verdana</span><span class="cssdelimitercolor" style="color: black">;</span></font><span class="csspropertycolor" style="color: red; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="red" face="Consolas, courier new" size="15px"><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"/>&nbsp;&nbsp;font-size</font></span><span class="cssdelimitercolor" style="color: black"><font color="black" face="Consolas, courier new" size="15px">:</font></span><font color="black" face="Consolas, courier new" size="15px"><span>&nbsp;</span><span class="csspropertyvaluecolor" style="color: black">20px</span><span class="cssdelimitercolor" style="color: black">;</span></font><span class="csspropertycolor" style="color: red; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="red" face="Consolas, courier new" size="15px"><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"/></font></span><span class="cssdelimitercolor" style="color: black; font-family: Consolas, courier new; font-size: 15px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="black" face="Consolas, courier new" size="15px">}</font></span>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="try it your self" ID="ID_1352518049" CREATED="1606113761676" MODIFIED="1606113769978"/>
+</node>
+</node>
+<node TEXT="CSS Examples" ID="ID_983795562" CREATED="1606113795534" MODIFIED="1606113796391">
+<node TEXT="over 300 examples" ID="ID_1375119851" CREATED="1606113809420" MODIFIED="1606113809894"/>
+<node TEXT="Go to CSS Examples!" ID="ID_1466227534" CREATED="1606113815268" MODIFIED="1606113815612"/>
+</node>
+<node TEXT="Use the Menu" ID="ID_830809232" CREATED="1606113827754" MODIFIED="1606113828182">
+<node TEXT="menu in large scree" ID="ID_1580850755" CREATED="1606113829545" MODIFIED="1606113834310">
+<node TEXT="on left" ID="ID_451746214" CREATED="1606113841934" MODIFIED="1606113844042"/>
+</node>
+<node TEXT="menu in small screen" ID="ID_1046077343" CREATED="1606113834877" MODIFIED="1606113839817">
+<node TEXT="on hamburger menu" ID="ID_1280567964" CREATED="1606113846000" MODIFIED="1606113855396"/>
+</node>
+</node>
+<node TEXT="CSS Templates" ID="ID_1525859741" CREATED="1606113863862" MODIFIED="1606113865437">
+<node TEXT="responsive css template" ID="ID_980636379" CREATED="1606113879834" MODIFIED="1606113888348"/>
+<node TEXT="Free CSS Templates!" ID="ID_208578551" CREATED="1606113888554" MODIFIED="1606113889695"/>
+</node>
+<node TEXT="CSS Exercises and Quiz Test" ID="ID_1843770570" CREATED="1606113899513" MODIFIED="1606113900231">
+<node TEXT="test your skill" ID="ID_1579999622" CREATED="1606113902101" MODIFIED="1606113909541">
+<node TEXT="exercise" ID="ID_45551459" CREATED="1606113915752" MODIFIED="1606113918844"/>
+<node TEXT="quiz" ID="ID_481420124" CREATED="1606113919020" MODIFIED="1606113920691"/>
+</node>
+</node>
+<node TEXT="CSS References" ID="ID_1203835343" CREATED="1606113931293" MODIFIED="1606113931651">
+<node TEXT="properties" ID="ID_814231756" CREATED="1606113973115" MODIFIED="1606113976435"/>
+<node TEXT="selector" ID="ID_191911712" CREATED="1606113976589" MODIFIED="1606113978174"/>
+<node TEXT="function" ID="ID_1066868304" CREATED="1606113981311" MODIFIED="1606113983145"/>
+<node TEXT="animatable" ID="ID_881131675" CREATED="1606113987965" MODIFIED="1606113991380"/>
+<node TEXT="aural" ID="ID_892996993" CREATED="1606113991551" MODIFIED="1606113997758"/>
+<node TEXT="unit" ID="ID_554630336" CREATED="1606113997940" MODIFIED="1606113999252"/>
+<node TEXT="color" ID="ID_1172161405" CREATED="1606114003325" MODIFIED="1606114004561"/>
+<node TEXT="default value" ID="ID_278097871" CREATED="1606114010522" MODIFIED="1606114013074"/>
+<node TEXT="browser support" ID="ID_1053805102" CREATED="1606114023133" MODIFIED="1606114026816"/>
+</node>
+<node TEXT="CSS Exam - Get Your Diploma!" ID="ID_1398903731" CREATED="1606114034167" MODIFIED="1606114034549">
+<node TEXT="Get Your Certificate" ID="ID_249825723" CREATED="1606114061444" MODIFIED="1606114068229"/>
+</node>
+</node>
+</node>
 </node>
 </map>
